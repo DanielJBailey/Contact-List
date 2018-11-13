5118da7449c53f6540f63734de4b2421040936cc
Daniel Bailey <danielbailey@Daniels-MacBook-Pro.local>
2018-11-13 12:26:55 -0700

    add validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,35 @@
 class Contact < ApplicationRecord
-    has_many :notes
+    has_many :notes, dependent: :destroy
+    has_one :address, dependent: :destroy
 end
+
+# VALIDATION
+# class Coffee < ActiveRecord::Base
+#     validates :size, inclusion: {
+#         in: %w(small medium large XL),
+#         message: "%{value} is not a valid size."
+#     }
+# end
+
+# class Person < ActiveRecord::Base
+#     validates :name, length: { minimum: 2 }
+#     validates :bio, length: { maximum: 500 }
+#     validates :password, length: { in: 6..20 } #shorthand for in between 6 and 20 characters
+#     validates :registration_number, length: { is: 6 }
+# end
+
+# class Player < ActiveRecord::Base
+#     validates :points, numericallity: true
+#     validates :games_played, numericallity: { only_integer: true }
+#     validates :golf_handicap, numericallity: { less_than_or_equal_to: 40.4, greater_than_or_equal_to: 2 }
+# end
+
+# class Person < ActiveRecord::Base
+#     validates :name, :login, :email, presence: true
+# end
+
+# class Account < ActiveRecord::Base
+#     validates :email, uniqueness: true
+# end
+
                         
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_183245) do
+ActiveRecord::Schema.define(version: 2018_11_13_185916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "state"
+    t.string "city"
+    t.string "zip"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_addresses_on_contact_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -33,5 +43,6 @@ ActiveRecord::Schema.define(version: 2018_11_13_183245) do
     t.index ["contact_id"], name: "index_notes_on_contact_id"
   end
 
+  add_foreign_key "addresses", "contacts"
   add_foreign_key "notes", "contacts"
 end
